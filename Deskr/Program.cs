4c207724c3255a41aba41063934856701bc7d092
Olive568 <luislabapis@gmail.com>
2024-01-15 17:21:14 +0800

    Add project files.

---
+++ Deskr/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deskr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Display display = new Display();
+            Start();
+          display.Disp();
+          Console.ReadKey();
+        }
+        static void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("                                                \r\n                                                \r\n`7MM\"\"\"Yb.                   `7MM               \r\n  MM    `Yb.                   MM               \r\n  MM     `Mb  .gP\"Ya  ,pP\"Ybd  MM  ,MP'`7Mb,od8 \r\n  MM      MM ,M'   Yb 8I   `\"  MM ;Y     MM' \"' \r\n  MM     ,MP 8M\"\"\"\"\"\" `YMMMa.  MM;Mm     MM     \r\n  MM    ,dP' YM.    , L.   I8  MM `Mb.   MM     \r\n.JMMmmmdP'    `Mbmmd' M9mmmP'.JMML. YA..JMML.   \r\n                                                \r\n                                                ");
+            Console.WriteLine();
+            Console.WriteLine("A. Create New Task");
+            Console.WriteLine("B. Verify Task");
+            Console.WriteLine("C. View Tasks");
+            Console.WriteLine("View Verification Status");
+
+        }
+    }
+}
