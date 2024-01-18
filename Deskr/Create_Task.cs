ce0f027e9d46daffd9081c883ab432f78600649f
Olive568 <luislabapis@gmail.com>
2024-01-18 17:07:06 +0800

    fixing adding taskss

--- Deskr/Create_Task.cs
+++ Deskr/Create_Task.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,22 +15,26 @@ namespace Deskr
     {
         public void Create()
         {
+            List<string[]> database = new List<string[]>();
             Console.Clear();
             string name = Name();
             string task = Task();
             DateTime time = DateTime.Now;
             string status = "In Progress";
+            database = Generate_Database();
+            int count = Count_Items() + 1;
+            database = Add_Items(database, count,status,time,task,name);
             Console.ReadKey();
         }
 
         private string Name()
         {
             string name = "";
-            while(true)
+            while (true)
             {
                 Console.WriteLine("Write the name");
                 name = Console.ReadLine();
-                if(name.Length <= 0)
+                if (name.Length <= 0)
                 {
                     Console.WriteLine("Invalid name");
                     Thread.Sleep(750);
@@ -39,16 +45,16 @@ namespace Deskr
                     break;
                 }
             }
-            
+
         }
         private string Task()
         {
             string task = "";
             Console.WriteLine("What is the name of the task? ");
-            while(true) 
+            while (true)
             {
                 task = Console.ReadLine();
-                if(task.Length < 6)
+                if (task.Length < 6)
                 {
                     Console.WriteLine("Invalid task");
                     Thread.Sleep(750);
@@ -60,7 +66,7 @@ namespace Deskr
                 }
             }
         }
-        private void Add(string name, string task, DateTime time, string status) 
+        private void Add(string name, string task, DateTime time, string status)
         {
             string line = "";
             int count = 0;
@@ -71,6 +77,61 @@ namespace Deskr
                     count++;
                 }
             }
+        }
+        private List<string[]> Generate_Database()
+        {
+            List<string[]> database = new List<string[]>();
+            int count = 0;
+            string fileName = "DeskrMain.csv";
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string[] data = new string[7];
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    data = line.Split(',');
+                    database[count] = data;
+                    count++;
+                }
+                
+            }
+            return database;
+        }
+        private int Count_Items()
+        {
+            int count = 0;
+            string fileName = "DeskrMain.csv";
+            string line = "";
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        private List<string[]> Add_Items(List<string[]> database, int count, string status, DateTime time, string task, string name)
+        {
+            string time_string = time.ToString();
+            database[count][0] = name;
+            database[count][1] = task;
+            database[count][2] = time_string;
+            database[count][3] = "NULL";
+            database[count][4] = status;
+            database[count][5] = "NULL";
+            database[count][6] = "NULL";
+            string filename = "DeskrMain.csv";
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if(i == 6)
+                        sw.Write(database[count][i]);
+                    sw.Write(database[count][i] + ",");
+                }
+            }
+            return database;
         }
     }
 }
