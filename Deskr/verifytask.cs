6de657712f361b9e627e008b38805b0c597c18c0
Adventure Morales <morales.225349@laspinas.sti.edu.ph>
2024-01-18 23:59:57 +0800

    did some more stufv on the verify task. Still pretty confused as to how the output is supposed to be

--- Deskr/verifytask.cs
+++ Deskr/verifytask.cs
@@ -3,27 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Deskr
 {
 
     internal class verifytask
     {
+        Program p = new Program();
+        static List<string> s = new List<string>();
+        
         public verifytask()
         {
             //Console.WriteLine(" ______     ______     ______   __  __     __     ______        ______     ______     __    __     __  __     __         ______    \r\n/\\  ___\\   /\\  __ \\   /\\  == \\ /\\ \\_\\ \\   /\\ \\   /\\  __ \\      /\\  == \\   /\\  __ \\   /\\ \"-./  \\   /\\ \\/\\ \\   /\\ \\       /\\  __ \\   \r\n\\ \\___  \\  \\ \\ \\/\\ \\  \\ \\  _-/ \\ \\  __ \\  \\ \\ \\  \\ \\  __ \\     \\ \\  __<   \\ \\ \\/\\ \\  \\ \\ \\-./\\ \\  \\ \\ \\_\\ \\  \\ \\ \\____  \\ \\ \\/\\ \\  \r\n \\/\\_____\\  \\ \\_____\\  \\ \\_\\    \\ \\_\\ \\_\\  \\ \\_\\  \\ \\_\\ \\_\\     \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\_____\\ \r\n  \\/_____/   \\/_____/   \\/_/     \\/_/\\/_/   \\/_/   \\/_/\\/_/      \\/_/ /_/   \\/_____/   \\/_/  \\/_/   \\/_____/   \\/_____/   \\/_____/ \r\n                                                                                                                                   ");
             //return "hello";
-            Console.WriteLine("What tasks would you like to verify?");
+            gatherinfo();
 
         }
-        public void Filereader(string filename)
+        public void gatherinfo()
         {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("What task would you like to verify?");
+            Console.WriteLine("What is your name: ");
+            string name = Console.ReadLine();
+            //s.Add(name);
+            Console.WriteLine("Verification Status (1: Verified, 2: For Revision");
+            int verification = int.Parse(Console.ReadLine());
+            string status = verifystatus(verification);
 
+            Console.WriteLine("Credentials: ");
+            string details = Console.ReadLine();
+            
+            Console.WriteLine("Comments:");
+            string comments = Console.ReadLine();
+
+            s.Add(name + "," + dateTime.ToString() + "," + status + "," + details + "," + comments);
+
+        }
+        public void Filereader()
+        {
+            using (StreamReader sr = new StreamReader(p.mainfile))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
         }
         
-        public void Filewriter()
+        public void Filewriter(string input)
         {
+            using (StreamWriter sw = new StreamWriter(p.verificationfile))
+            {
+                sw.WriteLine(input);
+            }
+        }
 
+        private string verifystatus(int choice)
+        {
+            switch (choice) 
+            {
+                case 1:
+                    return "Verified";
+                case 2:
+                    return "For Revision";
+                default:
+                    return "For Verification";
+            }
         }
     }
 }
